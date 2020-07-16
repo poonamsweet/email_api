239663e9eb9d3d502940fc99dd138b91a7a0342c
poonamsweet <poonamsweet>
2020-07-16 20:46:19 +0530

    first commit

--- api1/api1/settings.py
+++ api1/api1/settings.py
@@ -132,8 +132,8 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "poonamsweet672@gmail.com"
-EMAIL_HOST_PASSWORD = "Poonam@123"
+EMAIL_HOST_USER = "poonam@gmail.com"
+EMAIL_HOST_PASSWORD = "*****"
 
 
 AUTH_USER_MODEL = 'api.User'
